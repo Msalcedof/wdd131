07f8c2c18e1ed46eef39d908a7a4ae0947ebc9c1
salcedof <salcedomarcof91@gmail.com>
2024-11-17 21:30:13 -0600

    deleting codes from wrong folder

--- week01/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-// getdates.js
-
-// Set the current year dynamically in the footer
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Set the last modified date dynamically in the footer
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
